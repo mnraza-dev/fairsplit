ee1a46dfaa28403720db403baad7463895078ca8
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-05 02:31:27 +0530

    Refactor layout and footer components for improved styling and consistency

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,19 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+import { Inter } from "next/font/google";
+
+
+const inter = Inter({
   subsets: ["latin"],
 });
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
   title: "Fair Split",
   description: "Track. Split. Settle. Repeat",
@@ -30,7 +25,7 @@ export default function RootLayout({
         <link rel="icon" href="/favicon.ico" />
       </head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={` ${inter.className} `}
       >
         <Header/>
         <main className="min-h-screen ">
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Footer = () => {
     return (
 
-        <footer className="container mx-auto px-3 sm:px-6 lg:px-9">
-            <div className="flex items-center justify-between py-4">
+        <footer className="bg-green-800 py-6  mx-auto px-3 sm:px-6 lg:px-9  z-50 backdrop-blur shadow-md">
+            <div className="flex items-center justify-center py-4">
                 <div className="flex items-center gap-2">
-                    <p>© 2025 Fair Split. All rights reserved.</p>
+                    <p className='text-muted text-center text-sm'>© 2025 Fair Split. All rights reserved.</p>
                 </div>
             </div>
 
