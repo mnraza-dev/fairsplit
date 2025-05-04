1ab22c0dd8470f0fac32f8365fd5a0d8140bd4e5
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-05 01:56:09 +0530

    added footer

---
+++ components/Footer.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+
+        <footer className="container mx-auto px-3 sm:px-6 lg:px-9">
+            <div className="flex items-center justify-between py-4">
+                <div className="flex items-center gap-2">
+                    <p>© 2025 Fair Split. All rights reserved.</p>
+                </div>
+            </div>
+
+        </footer>
+    )
+}
+
+export default Footer
---
+++ components/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <div className="container mx-auto px-3 sm:px-6 lg:px-9">
+                <div className="flex items-center justify-between py-4">
+                    <div className="flex items-center gap-2">
+                     Fair Split
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
+
+export default Header
