3736a470d774be4d9754981eae47513986308d41
Md Noorullah Raza <mnraza94@gmail.com>
2025-05-05 04:19:58 +0530

    BarLoader added

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,7 @@
 "use client"
 import { useStoreUser } from '@/hooks/useStoreUser'
 import { useUser } from '@clerk/clerk-react'
-import Image from 'next/image'
+import BarLoader from 'react-spinners/BarLoader'
 import Link from 'next/link'
 import React from 'react'
 
@@ -11,7 +11,7 @@ const Header = () => {
     return (
 
         <header className="bg-white/95 sticky top-0 left-0  w-full mx-auto px-3 sm:px-6 lg:px-9 border-b  z-50 backdrop-blur shadow-md supports-[backdrop-filter]:bg-white/65">
-            <div className="flex items-center justify-between py-4">
+            <nav className="flex items-center justify-between py-4">
                 <div className="flex items-center gap-2">
                     <span className="text-2xl font-medium text-green-600">Fair </span>
                     <span className="text-2xl -ml-1 font-medium text-red-800">Split </span>
@@ -23,10 +23,14 @@ const Header = () => {
                     <Link href="/" className='text-gray-800'>Contact</Link>
 
                     <div>
-                        <Link href="/login" className='bg-green-400 text-white px-4 py-2 rounded-md hover:bg-green-500 transition duration-200'>Login</Link>
+                        <Link href="/sign-in" className='bg-green-400 text-white px-4 py-2 rounded-md hover:bg-green-500 transition duration-200'>Login</Link>
                     </div>
                 </div>
-            </div>
+            </nav>
+            {
+                isLoading && <BarLoader width={"100%"} color={"#22c55e"} /> // Show a loading spinner while the user is being stored
+
+            }
         </header>
 
     )
